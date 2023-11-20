44db50a85339b40fc528510734fd6a7d2d862e04
yuhengwu <799810767@qq.com>
2023-11-20 17:23:47 -0600

    Add stats to api

--- api.py
+++ api.py
@@ -19,6 +19,8 @@ import torch.backends.cudnn as cudnn
 
 import cv2
 
+import time
+
 def parse_result(det_result, img, score_threshold = 0.15, top_k = 15):
     h, w, _ = img.shape
 
@@ -44,7 +46,11 @@ def parse_result(det_result, img, score_threshold = 0.15, top_k = 15):
             num_dets_to_consider = j
             break
 
-    masks = masks[:num_dets_to_consider, :, :, None]
+    if len(masks) == 0:
+        masks = torch.reshape(masks, (0, h, w, 1))
+    else:
+        masks = masks[:num_dets_to_consider, :, :, None]
+
     class_names = []
     for j in range(num_dets_to_consider):
         class_names.append(cfg.dataset.class_names[classes[j]])
@@ -72,19 +78,44 @@ net.detect.use_fast_nms = True
 net.detect.use_cross_class_nms = False
 cfg.mask_proto_debug = False
 
+# a dictionary that store the time for each step
+#  - model
+#  - parseresult
+#  - center
+#  - contour
+#  - draw
+stats = {
+    'model': 0.0,
+    'parseresult': 0.0,
+    'center': 0.0,
+    'contour': 0.0,
+    'draw': 0.0,
+}
+stats_count = 0
+
 
 def process_image(image: np.ndarray, score_threshold = 0.15, top_k = 15):
-    global net
+    global net, stats, stats_count
+
+    stats_count += 1
+
+    start_time = time.time()
 
     # return the raw results
     frame = torch.from_numpy(image).cuda().float()
     batch = FastBaseTransform()(frame.unsqueeze(0))
     preds = net(batch)
 
+    stats['model'] += time.time() - start_time
+    start_time = time.time()
+
     # masks: torch.Tensor, [N, H, W, 1]
     # boxes: np.ndarray, [N, 4]
     # class_names: list, [N]
+    # scores: np.ndarray, [N]
     masks, boxes, class_names, scores = parse_result(preds, frame, score_threshold = score_threshold, top_k = top_k)
+    stats['parseresult'] += time.time() - start_time
+    start_time = time.time()
 
     # find geometry center of each mask using torch
     geometry_centers = []
@@ -95,6 +126,9 @@ def process_image(image: np.ndarray, score_threshold = 0.15, top_k = 15):
         y_center = torch.mean(y_indices.float()).item()
         x_center = torch.mean(x_indices.float()).item()
         geometry_centers.append([x_center, y_center])
+
+    stats['center'] += time.time() - start_time
+    start_time = time.time()
 
     masks = masks.cpu().numpy().astype(np.uint8)
 
@@ -108,8 +142,16 @@ def process_image(image: np.ndarray, score_threshold = 0.15, top_k = 15):
         contours = sorted(contours, key=lambda x: len(x[:, 0, :]), reverse=True)
         mask_contours.append(contours[0][:, 0, :].tolist())
 
+    stats['contour'] += time.time() - start_time
+
     # remove the last dimension of masks
-    masks = mask.squeeze(axis=3).tolist()
+    masks = masks.squeeze(axis=3).tolist()
+
+    # convert boxes to list
+    boxes = boxes.tolist()
+
+    # convert scores to list
+    scores = scores.tolist()
 
     return masks, mask_contours, boxes, class_names, scores, geometry_centers
     
@@ -144,12 +186,6 @@ def get_recognition(image: np.ndarray, filter_objects = [], score_threshold = 0.
             scores = new_scores
             geometry_centers = new_geometry_centers
 
-        
-        # convert masks, scores and boxes to list
-        masks = masks.tolist()
-        boxes = boxes.tolist()
-        scores = scores.tolist()
-
         return {
             'masks': masks,
             'mask_contours': mask_contours,
@@ -160,18 +196,20 @@ def get_recognition(image: np.ndarray, filter_objects = [], score_threshold = 0.
         }
     
 def show_recognition(image: np.ndarray, score_threshold = 0.15, top_k = 15):
+    global stats, stats_count
+
     with torch.no_grad():
         cudnn.fastest = True 
         torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
         masks, mask_contours, boxes, class_names, scores, geometry_centers = process_image(image, score_threshold = score_threshold, top_k = top_k)
 
+    start_time = time.time()
+
     # draw the masks
     # each mask is a H*W 0/1 matrix, so multiply by a color to get the color mask
     for i, mask in enumerate(masks):
-        # skip the first mask
-        if i == 0:
-            continue
+        mask = np.array(mask, dtype=np.uint8)
         color_mask = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
         color_mask[mask == 1] = [255, 0, 0]
         image = cv2.addWeighted(image, 1, color_mask, 0.5, 0)
@@ -181,9 +219,19 @@ def show_recognition(image: np.ndarray, score_threshold = 0.15, top_k = 15):
         cv2.circle(image, (int(center[0]), int(center[1])), 2, (0, 0, 255), -1)
         cv2.putText(image, class_names[i] + " " + str(scores[i]), (int(center[0]), int(center[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     
+    stats['draw'] += time.time() - start_time
+    if stats_count % 50 == 0:
+        print(stats)
+        stats = {
+            'model': 0.0,
+            'parseresult': 0.0,
+            'center': 0.0,
+            'contour': 0.0,
+            'draw': 0.0,
+        }
     # show the image
     cv2.imshow('image', image)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
 
 if __name__ == '__main__':
     image = cv2.imread('./p1.png')
